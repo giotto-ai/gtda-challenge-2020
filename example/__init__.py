c63fb95a6472d5fa905d4cecb74b119551011b18
giotto-learn <maintainers@giotto.ai>
2020-09-14 18:29:55 +0200

    allow local import and test via papermill

--- example/__init__.py
+++
@@ -1,9 +0,0 @@
-from .plotting import plot_diagram, plot_point_cloud, \
-    plot_crash_detections, plot_crash_comparisons
-
-__all__ = [
-    'plot_diagram',
-    'plot_point_cloud',
-    'plot_crash_detections',
-    'plot_crash_comparisons',
-    ]
