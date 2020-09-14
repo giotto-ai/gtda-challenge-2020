dbb7973d3ac81231dca77887ffc7d08cf488f0dc
giotto-learn <maintainers@giotto.ai>
2020-09-14 18:13:38 +0200

    add init file

---
+++ example/__init__.py
@@ -0,0 +1,9 @@
+from .plotting import plot_diagram, plot_point_cloud, \
+    plot_crash_detections, plot_crash_comparisons
+
+__all__ = [
+    'plot_diagram',
+    'plot_point_cloud',
+    'plot_crash_detections',
+    'plot_crash_comparisons',
+    ]
